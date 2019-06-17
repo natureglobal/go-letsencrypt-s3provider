2544de2ebc9ef4d98f105466af83ed1d9ed6d75d
Songmu <y.songmu@gmail.com>
2019-06-18 01:02:32 +0900

    migrate to offcial aws-sdk-go from mitchelh/goamz

--- provider.go
+++ provider.go
@@ -3,30 +3,34 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/go-acme/lego/challenge"
-	"github.com/mitchellh/goamz/aws"
-	"github.com/mitchellh/goamz/s3"
 )
 
 var (
-	Bucket *s3.Bucket
+	s3cli      *s3.S3
+	bucketName string
 )
 
 type s3UploadingProvider struct {
 }
 
 func init() {
-	auth, err := aws.EnvAuth()
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
 	if err != nil {
-		log.Fatalf("EnvAuth failed, error: %s", err)
+		log.Fatalf("session.NewSession failed, error: %s", err)
 	}
-	bucket := os.Getenv("AWS_LETSENCRYPT_S3PROVIDER_BUCKET")
-	if bucket == "" {
+	bucketName = os.Getenv("AWS_LETSENCRYPT_S3PROVIDER_BUCKET")
+	if bucketName == "" {
 		log.Fatalf("AWS_LETSENCRYPT_S3PROVIDER_BUCKET required")
 	}
-	client := s3.New(auth, aws.USEast)
-	Bucket = client.Bucket(bucket)
+	s3cli = s3.New(sess)
 }
 
 func NewS3UploadingProvider() challenge.Provider {
@@ -36,18 +40,28 @@ func NewS3UploadingProvider() challenge.Provider {
 func (p s3UploadingProvider) Present(domain, token, keyAuth string) error {
 	log.Printf("Present domain: %s\ntoken: %s\nkeyAuth: %s", domain, token, keyAuth)
 
-	if err := Bucket.Put(token, []byte(keyAuth), "text/plain", s3.Private); err != nil {
+	if _, err := s3cli.PutObject(&s3.PutObjectInput{
+		ACL:         aws.String(s3.BucketCannedACLPrivate),
+		Body:        strings.NewReader(keyAuth),
+		ContentType: aws.String("text/plain"),
+		Bucket:      aws.String(bucketName),
+		Key:         aws.String(token),
+	}); err != nil {
 		log.Printf("Put: %s failed, error: %s", token, err)
+		return err
 	}
-
 	return nil
 }
 
 func (p s3UploadingProvider) CleanUp(domain, token, keyAuth string) error {
 	log.Printf("CleanUp domain: %s\ntoken: %s\nkeyAuth: %s", domain, token, keyAuth)
 
-	if err := Bucket.Del(token); err != nil {
+	if _, err := s3cli.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(token),
+	}); err != nil {
 		log.Printf("Del: %s failed, error: %s", token, err)
+		return err
 	}
 	return nil
 }
